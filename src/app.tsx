fb6ee0efdd07990b444f970e6c77a200d484ec05
ismael cezar <103972495+ismaelczar@users.noreply.github.com>
2025-07-09 21:06:13 -0300

    finished

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,21 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { CreateRomm } from './pages/create-room';
-import { Room } from './pages/rooms';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { CreateRoom } from './pages/create-room'
+import { RecordRoomAudio } from './pages/record-room-audio'
+import { Room } from './pages/room'
+
+const queryClient = new QueryClient()
 
 export function App() {
-  const queryClient = new QueryClient();
-
   return (
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <Routes>
-          <Route element={<CreateRomm />} index />
-          <Route element={<Room />} path="/room/:id" />
+          <Route element={<CreateRoom />} index />
+          <Route element={<Room />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
-  );
+  )
 }
